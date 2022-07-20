3a4f19f47c2aeadb4ac8bf0004456da726fc96e3
leonildoalbani <leonildo.albani@gmail.com>
2022-07-19 22:43:58 -0300

    add Depends example

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from typing import Union
+from fastapi import FastAPI, Depends
 from src.application.routers import users
 
 
@@ -8,3 +9,13 @@ app.include_router(users.router)
 @app.get("/")
 async def home():
     return {"status": "ok"}
+
+class CommonQueryParams:
+    def __init__(self, q: Union[str, None] = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+
+@app.get("/search")
+async def search(commons: CommonQueryParams = Depends(CommonQueryParams)):
+    return {"commons": commons}
