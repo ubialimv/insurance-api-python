1ce2f99febe86b37bf98850322265fbae9a4a341
leonildoalbani <leonildo.albani@gmail.com>
2022-07-19 22:28:53 -0300

    Add example of "Depends"

--- src/application/routers/users.py
+++ src/application/routers/users.py
@@ -1,19 +1,24 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 from src.domain.user.user_entity import User, UserDTO
 
+
+# async def get_query_token(tokenzinho: str):
+#     print('passou')
+#     if tokenzinho != "jessica":
+#         raise HTTPException(status_code=400, detail="No Jessica token provided")
 
 router = APIRouter(
     prefix="/users",
     tags=["users"],
-    # TODO dependencies=[Depends(get_token_header)],
+    # dependencies=[Depends(get_query_token)],
     responses={404: {"description": "Não encontrado"}},
 )
 
-@router.post("/", description="Api to save user", status_code=201)
+@router.post("", description="Api to save user", status_code=201)
 async def save_user(user_props: UserDTO):
     user = User(user_props)
     return {"user": user}
 
-@router.get("/", description="Api to list users")
+@router.get("", description="Api to list users")
 async def users():
     return [User(user_props=UserDTO(age=0))]
